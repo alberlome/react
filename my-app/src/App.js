38c38d3ea31d7814d7effb8f986e63d5878df79c
Alberto David López Mesas <alberlome@gmail.com>
2023-03-14 14:15:01 +0100

    Merge button click result

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,53 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-const user = {
-  name: "Hedy Lamarr",
-  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 90,
-};
-
-const products = [
-  { title: "Cabbage", id: 1 },
-  { title: "Garlic", id: 2 },
-  { title: "Apple", id: 3 },
-];
-
-const listItems = products.map((product) => {
-  return <li key={product.id}>{product.title}</li>;
-});
-
-function MyButton() {
+function App() {
   const [count, setCount] = useState(0);
+
   function handleClick() {
     setCount(count + 1);
   }
+
   return (
-    <button onClick={handleClick}>My Button clicked {count} times </button>
+    <div className="App">
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
   );
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="title-app">
-        Welcome to my new app <span className="user-app">{user.name}</span>
-      </h1>
-      <img
-        className="img-app"
-        src={user.imageUrl}
-        alt={"Photo of " + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-      <ul>{listItems}</ul>
-      <MyButton />
-      <MyButton />
-      <MyButton />
-    </div>
-  );
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>My Button clicked {count} times</button>;
 }
 
 export default App;
